2283ad5806786c221dc2bb397d9c8a6e1b8b06c1
mreyilmz <mre.ylmaz@gmail.com>
2024-01-27 18:57:39 +0300

    first commit

---
+++ ExampleAPI/Controllers/UsersController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using ExampleAPI.Entities;
+using ExampleAPI.Repositories.Abstracts;
+using ExampleAPI.Repositories.Concretes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using IdentityService;
+using static IdentityService.KPSPublicSoapClient;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ExampleAPI.Controllers;
+
+[Route("api/[controller]")]
+public class UsersController : Controller
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IAccountTransactionRepository _accountTransactionRepository;
+
+    public UsersController(
+        IUserRepository userRepository,
+        IAccountTransactionRepository accountTransactionRepository)
+    {
+        _userRepository = userRepository;
+        _accountTransactionRepository = accountTransactionRepository;
+    }
+
+    [HttpGet("GetAll")]
+    public IActionResult GetAll()
+    {
+        return Ok(_userRepository.GetAll());
+    }
+    [HttpGet("GetAllWithBalanceTransactions")]
+    public IActionResult GetAllWithBalanceTransactions()
+    {
+        return Ok(_userRepository.GetAll(
+            include:user=>user.Include(u=>u.AccountTransactions)
+            ));
+    }
+    [HttpGet("GetAllWithOrders")]
+    public IActionResult GetAllWithOrders()
+    {
+        return Ok(_userRepository.GetAll(
+            include: user => user
+                    .Include(u => u.Orders).ThenInclude(o=>o.OrderDetails).ThenInclude(od=>od.ProductTransaction)
+                    .Include(u=>u.Orders).ThenInclude(o=>o.OrderDetails).ThenInclude(od=>od.Product).ThenInclude(p=>p.Category)
+            ));
+    }
+    [HttpGet("GetAllWithAllDetails")]
+    public IActionResult GetAllWithAllDetails()
+    {
+        return Ok(_userRepository.GetAll(
+            include: user => user
+                    .Include(u => u.Orders).ThenInclude(o => o.OrderDetails).ThenInclude(od => od.ProductTransaction)
+                    .Include(u => u.Orders).ThenInclude(o => o.OrderDetails).ThenInclude(od => od.Product).ThenInclude(p => p.Category)
+                    .Include(u=>u.AccountTransactions)
+            ));
+    }
+
+    [HttpGet("GetById/{id}")]
+    public IActionResult Get(Guid id)
+    {
+        return Ok(_userRepository.Get(user=>user.Id==id));
+    }
+
+
+    [HttpPost("Add")]
+    public async Task<IActionResult> Add([FromBody] User user)
+    {
+        using (KPSPublicSoapClient soapClient = new(EndpointConfiguration.KPSPublicSoap12))
+        {
+            var result = await soapClient.TCKimlikNoDogrulaAsync(
+                TCKimlikNo: (long)Convert.ToDouble(user.IdentificationNumber),
+                Ad: user.FirstName,
+                Soyad: user.LastName,
+                DogumYili: user.BirthYear
+            );
+            if (!result.Body.TCKimlikNoDogrulaResult)
+                return BadRequest("There is no citizen matching to provided identity informations in the government system.");
+            else
+                return Ok(_userRepository.Add(user));
+        };
+    }
+
+
+    [HttpPost("AddBalance")]
+    public IActionResult Add([FromBody] AccountTransaction accountTransaction)
+    {
+        return Ok(_accountTransactionRepository.Add(accountTransaction));
+    }
+
+    [HttpPut("Update")]
+    public IActionResult Update([FromBody] User user)
+    {
+        return Ok(_userRepository.Update(user));
+    }
+
+    [HttpDelete("DeleteById/{id}")]
+    public IActionResult Delete(Guid id)
+    {
+        var user = _userRepository.Get(user => user.Id == id);
+        if (user == null) return BadRequest("User not found");
+        return Ok(_userRepository.Delete(user));
+    }
+}
+
